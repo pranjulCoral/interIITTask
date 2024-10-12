524a75841dc6ed5133a4d623cee5b432830e2a59
pranjulCoral <pranjul@coralacademy.com>
2024-10-13 04:22:26 +0530

    almost done

--- godowns/src/app/lib/mongodb.ts
+++ godowns/src/app/lib/mongodb.ts
@@ -1,23 +1,16 @@
 // deno-lint-ignore-file no-node-globals
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/* eslint-disable @typescript-eslint/no-unused-expressions  */
 import { MongoClient } from 'mongodb';
-import process from 'node:process';
 
 const uri = "mongodb+srv://PranjulShukla:beena55@cluster0.qicx6ls.mongodb.net/interIITdb" // Make sure to set your MongoDB URI in .env
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable to keep the MongoDB client instance
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  // In production mode, create a new client instance
   client = new MongoClient(uri);
   clientPromise = client.connect();
-}
+
 
 export default clientPromise;
